bd1a4c23f237a36c6be5f3b6daba2ffd9830b168
Alex Paredes <alexparedesr95@gmail.com>
2022-04-05 16:58:53 -0500

    FEAT: Added examples of querys with WHERE.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, onSnapshot, query, setDoc, updateDoc, where } from "firebase/firestore";
 import { db } from "./firebase/config";
 import { retornaDocumentos } from "./helpers/mostrar-documentos";
 
@@ -58,4 +58,21 @@ const usuariosRef = collection( db, 'usuarios' );
 
 /* getDocs( usuariosRef )
     .then( retornaDocumentos ); */
+
+// QUERYS CON WHERE.
+    
+// SELECT * FROM usuarios WHERE activo = true
+const queryUsuariosActivos = query( usuariosRef, where( 'activo', '==', true ) );
+
+// SELECT * FROM usuarios WHERE salario BETWEEN 1800 AND 2300
+const querySalario1800Y2300 = query( usuariosRef, where( 'salario', '>=', 1800 ), where( 'salario', '<=', 2300 ) );
+
+// SELECT * FROM usuarios WHERE salario > 1800 AND activo = true
+const querySalario1800Activo = query( usuariosRef, where( 'salario', '>', 1800 ), where( 'activo', '==', true ) );
+
+const querySnapshot = getDocs( querySalario1800Activo );
+
+querySnapshot
+    .then( retornaDocumentos )
+    .catch( console.log );
 
