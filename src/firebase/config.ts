4c4168d9f6fd315046ff489cad43bcc750494fd0
Alex Paredes <alexparedesr95@gmail.com>
2022-04-05 12:50:44 -0500

    INIT: Project base.

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpNorUBtznYeOKK1DUcoaKloWQnw-jjvI",
+  authDomain: "sql-demos-53d3b.firebaseapp.com",
+  projectId: "sql-demos-53d3b",
+  storageBucket: "sql-demos-53d3b.appspot.com",
+  messagingSenderId: "908085837467",
+  appId: "1:908085837467:web:1ad606154034e231cb103e"
+};
+
+const app = initializeApp(firebaseConfig);
